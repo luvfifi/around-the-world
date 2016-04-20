28f1f1292fbda6be9bd0b9e17c35afd87a784284
luvfifi <luvallah4life@gmai.com>
2016-04-20 13:44:21 +0000

    around the world contagious 5 cuntreies

---
+++ js.js
@@ -0,0 +1,36 @@
+
+var country ={
+  YEMEN:{
+      Capital:"sana'a",
+     "National Animal":"Arabian leopard",
+     "National Language":"Arabic",
+      Population: "24.41 million (2013)"
+  },
+      
+      NEPAL:{
+      Capital:"Kathmandu",
+     "National Animal":"cow",
+     "National Language":"Nepali",
+      Population: "27.8 million (2013)"
+  }, 
+  
+  TURKEY:{
+      Capital:"Ankara",
+     "National Animal":"The Grey Wolf",
+     "National Language":"Turkish",
+      Population: "74.93 million (2013)"
+  }, 
+  
+   FRANCE:{
+      Capital:"Paris",
+     "National Animal":"Beaver (Canada) ",
+     "National Language":"French",
+      Population: "66.03 million (2013)"
+  }, 
+  LEBANON:{
+      Capital:"Berut",
+     "National Animal":"Striped Hyena",
+     "National Language":"Arabic",
+      Population: "4.467 million (2013)"
+  }
+};
